05b4dcf046c020e6f2f424facc6f0487175b33ab
nghilt7 <nghilt2307@gmail.com>
2022-03-08 09:17:38 +0700

    Add authorization for admin, qac, qam, staff.

--- 1640_Project/Filters/AdminAuthorization.cs
+++ 1640_Project/Filters/AdminAuthorization.cs
@@ -10,6 +10,7 @@ namespace _1640_Project.Filters
     {
         public void OnAuthorization(AuthorizationContext filterContext)
         {
+            // Nghi
             if (filterContext.RequestContext.HttpContext.Session["IsAdmin"] == null)
             {
                 filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new { controller = "Home", action = "Permission" }));
