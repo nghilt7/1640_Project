59166059569ec63d52f0a2447beba8ae7f74200d
Hon <kimhon0705@gmail.com>
2022-03-15 13:49:05 +0700

    As a admin, I want to download csv file when system close(Submission)

--- 1640_Project/Areas/QAM/Controllers/DownController.cs
+++ 1640_Project/Areas/QAM/Controllers/DownController.cs
@@ -21,12 +21,19 @@ namespace _1640_Project.Areas.QAM.Controllers
         IdeasDbContext db = new IdeasDbContext();
         public ActionResult Index()
         {
-            List<Idea> Ideas = db.Ideas.ToList();
-            return View(Ideas);
+            List<Submission> submissions = db.Submissions.ToList();
+            return View(submissions);
+        }
+
+        public ActionResult Down(int id)
+        {
+            List<Idea> ideas = db.Ideas.Where(t => t.SubmissionID == id).ToList();
+            ViewBag.SubID = id;
+            return View(ideas);
         }
 
         [HttpPost]
-        public FileResult ExportToExcel()
+        public FileResult ExportToExcel(int SubID)
         {
             DataTable dt = new DataTable("Grid");
             dt.Columns.AddRange(new DataColumn[9] { new DataColumn("IdeaID"),
@@ -39,7 +46,7 @@ namespace _1640_Project.Areas.QAM.Controllers
                                                      new DataColumn("CategoryID"),
                                                      new DataColumn("SubmissionID")});
 
-            var Idea = from IdeasDbContext in db.Ideas select IdeasDbContext;
+            var Idea = from IdeasDbContext in db.Ideas.Where(t => t.SubmissionID == SubID) select IdeasDbContext;
 
             foreach (var insurance in Idea)
             {
@@ -57,100 +64,6 @@ namespace _1640_Project.Areas.QAM.Controllers
                     return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ExcelFile.xlsx");
                 }
             }
-        }
-
-        [HttpPost]
-        public ActionResult ImportFromExcel(HttpPostedFileBase postedFile)
-        {
-            if (ModelState.IsValid)
-            {
-                if (postedFile != null && postedFile.ContentLength > (1024 * 1024 * 50))  // 50MB limit  
-                {
-                    ModelState.AddModelError("postedFile", "Your file is to large. Maximum size allowed is 50MB !");
-                }
-
-                else
-                {
-                    string filePath = string.Empty;
-                    string path = Server.MapPath("~/Uploads/");
-                    if (!Directory.Exists(path))
-                    {
-                        Directory.CreateDirectory(path);
-                    }
-
-                    filePath = path + Path.GetFileName(postedFile.FileName);
-                    string extension = Path.GetExtension(postedFile.FileName);
-                    postedFile.SaveAs(filePath);
-
-                    string conString = string.Empty;
-                    switch (extension)
-                    {
-                        case ".xls": //For Excel 97-03.  
-                            conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
-                            break;
-                        case ".xlsx": //For Excel 07 and above.  
-                            conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
-                            break;
-                    }
-
-                    try
-                    {
-                        DataTable dt = new DataTable();
-                        conString = string.Format(conString, filePath);
-
-                        using (OleDbConnection connExcel = new OleDbConnection(conString))
-                        {
-                            using (OleDbCommand cmdExcel = new OleDbCommand())
-                            {
-                                using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
-                                {
-                                    cmdExcel.Connection = connExcel;
-
-                                    //Get the name of First Sheet.  
-                                    connExcel.Open();
-                                    DataTable dtExcelSchema;
-                                    dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                                    string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
-                                    connExcel.Close();
-
-                                    //Read Data from First Sheet.  
-                                    connExcel.Open();
-                                    cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
-                                    odaExcel.SelectCommand = cmdExcel;
-                                    odaExcel.Fill(dt);
-                                    connExcel.Close();
-                                }
-                            }
-                        }
-
-                        conString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
-                        using (SqlConnection con = new SqlConnection(conString))
-                        {
-                            using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
-                            {
-                                //Set the database table name.  
-                                sqlBulkCopy.DestinationTableName = "InsuranceCertificate";
-                                con.Open();
-                                sqlBulkCopy.WriteToServer(dt);
-                                con.Close();
-                                return Json("File uploaded successfully");
-                            }
-                        }
-                    }
-
-                    //catch (Exception ex)  
-                    //{  
-                    //    throw ex;  
-                    //}  
-                    catch (Exception e)
-                    {
-                        return Json("error" + e.Message);
-                    }
-                    //return RedirectToAction("Index");  
-                }
-            }
-            //return View(postedFile);  
-            return Json("no files were selected !");
         }
 
     }
